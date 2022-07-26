4b03d0f418132b250829e8648dccb94fde2b58ce
Daniel Chenery <daniel@itsdaniel0.com>
2022-07-26 18:27:46 +0100

    AnyAndNotNull

---
+++ Extensions.Tests/EnumerableExtensionsTests.cs
@@ -0,0 +1,44 @@
+namespace Extensions.Tests
+{
+    public class EnumerableExtensionsTests
+    {
+        [Fact]
+        public void AnyAndNotNull_WhenNull_ReturnsFalse()
+        {
+            // Arrange
+            var enumerable = default(IEnumerable<object>);
+
+            // Act
+            var any = enumerable.AnyAndNotNull();
+
+            // Assert
+            Assert.False(any);
+        }
+
+        [Fact]
+        public void AnyAndNotNull_WhenEmpty_ReturnsFalse()
+        {
+            // Arrange
+            var enumerable = Enumerable.Empty<object>();
+
+            // Act
+            var any = enumerable.AnyAndNotNull();
+
+            // Assert
+            Assert.False(any);
+        }
+
+        [Fact]
+        public void AnyAndNotNull_WhenPopulated_ReturnsTrue()
+        {
+            // Arrange
+            var enumerable = Enumerable.Repeat(default(object), 10);
+
+            // Act
+            var any = enumerable.AnyAndNotNull();
+
+            // Assert
+            Assert.True(any);
+        }
+    }
+}
---
+++ Extensions/EnumerableExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static bool AnyAndNotNull<T>(this IEnumerable<T> enumerable)
+            => enumerable?.Any() == true;
+    }
+}
