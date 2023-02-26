6b5b41244a2e1c491c7db244fa15ffea4c453abf
Daniel Chenery <daniel@itsdaniel0.com>
2023-02-26 11:32:21 +0000

    Added comments

--- X10sions/BooleanExtensions.cs
+++ X10sions/BooleanExtensions.cs
@@ -2,6 +2,11 @@
 {
     public static class BooleanExtensions
     {
+        /// <summary>
+        /// Inverts the current boolean.
+        /// </summary>
+        /// <param name="value">The boolean to invert</param>
+        /// <returns>False when true. True when false</returns>
         public static bool Invert(this bool value) => !value;
     }
 }
